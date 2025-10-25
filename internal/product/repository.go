c2a3feb443cc434105f04f7e37ecead9e4d52b54
CaioMtho <caiomathol@gmail.com>
2025-10-25 10:30:15 -0300

    feat: repository de product

---
+++ internal/product/repository.go
@@ -0,0 +1,71 @@
+package product
+
+import (
+	"database/sql"
+	"github.com/CaioMtho/unstock/internal/config"
+)
+
+func rowsToProducts(rows *sql.Rows) ([]Product, error){
+	var products []Product
+	for rows.Next() {
+		var p Product
+        err := rows.Scan(&p.ID, &p.Name, &p.Price, &p.Stock, &p.MinStock)
+        if err != nil {
+            return nil, err
+        }
+        products = append(products, p)
+    }
+
+    return products, nil
+}
+
+func GetAllProducts() ([]Product, error) {
+    rows, err := config.DB.Query("SELECT * FROM products")
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    products, err := rowsToProducts(rows)
+	return products, err
+}
+
+func GetProductById(id int) (Product, error) {
+    var p Product
+    stmt := "SELECT id, name, price, stock, min_stock FROM products WHERE id = ?"
+    err := config.DB.QueryRow(stmt, id).Scan(&p.ID, &p.Name, &p.Price, &p.Stock, &p.MinStock)
+    if err != nil {
+        return Product{}, err
+    }
+    return p, nil
+}
+
+func InsertProduct (p Product) error {
+	stmt := `INSERT INTO products (name, price, stock, min_stock) VALUES (?, ?, ?, ?)`
+	_, err := config.DB.Exec(stmt, p.Name, p.Price, p.Stock, p.MinStock)
+	return err
+}
+
+
+func GetLowStockProducts() ([]Product, error){
+	rows, err := config.DB.Query("SELECT * FROM products WHERE MinStock > Stock ORDER BY Stock")
+	if err != nil {
+		return nil, err
+	}
+
+	products, err := rowsToProducts(rows)
+
+    return products, err
+}
+
+func DeleteProductById(id int) error {
+	stmt := "UPDATE TABLE products WHERE id = ? SET is_active = 0"
+	_, err := config.DB.Exec(stmt, id);
+	return err
+}
+
+func UpdateProduct(p Product) error {
+	stmt := `UPDATE products SET name = ?, price = ?, stock = ?, min_stock = ? WHERE id = ?`
+	_, err := config.DB.Exec(stmt, p.Name, p.Price, p.Stock, p.MinStock, p.ID)
+	return err
+}
--- scripts/01_create.sql
+++ scripts/01_create.sql
@@ -1,7 +1,12 @@
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     price REAL NOT NULL,
     stock INT NOT NULL,
-    min_stock INT NOT NULL
+    min_stock INT NOT NULL,
+    is_active INT 
 );
+
+CREATE INDEX IF NOT EXISTS idx_products_name ON products (name);
+CREATE INDEX IF NOT EXISTS idx_products_min_stock ON products (min_stock);
+CREATE INDEX IF NOT EXISTS idx_products_is_active ON products (is_active);
